5803956fb3ffead7ca48134703f5cd121cefe16a
Alina483 <alina-demonti@hotmail.com>
2025-11-01 20:55:23 +0000

    More Feautures

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import Die from '../src/components/Die'
 
 export default function App() {
   const [dice, setDice] = useState(generateAllNewDice())
+  const [gameWon, setGameWon] = useState(false)
+
+  if (dice.every(die => die.isHeld) && dice.every(die => die.value === dice[0].value)) {
+        setGameWon(true)
+  }
 
   function generateAllNewDice() {
     return new Array(10)
@@ -11,13 +16,21 @@ export default function App() {
         isHeld: false,
         id: crypto.randomUUID()
       }))
-}
+  }
 
-function hold(id) {
+  function hold(id) {
     setDice(oldDice => oldDice.map((dieObj) => {
-        return dieObj.id === id ? {...dieObj, isHeld: !dieObj.isHeld} : dieObj
+      return dieObj.id === id ? {...dieObj, isHeld: !dieObj.isHeld} : dieObj
     }))
-}
+  }
+
+  function rollDice() {
+    setDice(oldDice => oldDice.map(dieObj => {
+      return dieObj.isHeld
+        ? dieObj
+        : { ...dieObj, value: Math.ceil(Math.random() * 6) };
+    }))
+  }
 
   const diceElements = dice.map(dieObj => <Die key={dieObj.id} value={dieObj.value} isHeld={dieObj.isHeld} holdDice={() => hold(dieObj.id)} />)
 
@@ -26,17 +39,14 @@ function hold(id) {
       <div className="container">
         {diceElements}
       </div>
-      <button className="roll-dice" onClick={()=>setDice(generateAllNewDice())}>Roll Dice</button>
+      <button className="roll-dice" onClick={rollDice}>{gameWon ? "New Game" : "Roll"}</button>
     </main>
   )
 }
 
 /**
-     * Challenge: Update the `rollDice` function to not just roll
-     * all new dice, but instead to look through the existing dice
-     * to NOT role any that are being `held`.
-     * 
-     * Hint: this will look relatively similiar to the `holdDice`
-     * function below. When we're "rolling" a die, we're really
-     * just updating the `value` property of the die object.
+     * Challenge part 2:
+     * 1. Create a new `gameWon` variable.
+     * 2. If `gameWon` is true, change the button text to
+     *    "New Game" instead of "Roll"
      */
