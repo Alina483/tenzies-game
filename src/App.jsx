e55a2dde74414dced9dd2146554d7a23967eae68
Alina483 <alina-demonti@hotmail.com>
2025-11-01 12:53:58 +0000

    First changes

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+export default function App() {
+
+  return (
+    <main>
+      <h1>Welcome to React!</h1>
+    </main>
+  )
+}
+/**
+ * Challenge: Start a brand new React app!
+ * - Create a separate App component
+ * - Import and render the App component here
+ * - In the App component, render a <main> element
+ * - Style everything to look like the slide
+ */
