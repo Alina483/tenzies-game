a410ab0a1faf33863554223b3572993b0a20a0de
Alina483 <alina-demonti@hotmail.com>
2025-11-02 14:11:16 +0000

    Dice face

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,5 +1,19 @@
+const pipMap = {
+  1: [4],
+  2: [0, 8],
+  3: [0, 4, 8],
+  4: [0, 2, 6, 8],
+  5: [0, 2, 4, 6, 8],
+  6: [0, 2, 3, 5, 6, 8]
+};
+
 export default function Die(prop) {
     return (
-        <button className={`die ${prop.isHeld ? "held" : "not-held"}`} style={{ backgroundColor: prop.isHeld && "#59E391" }} onClick={prop.holdDice}>{prop.value}</button>
-    )
+        <div className={`die-face${prop.isHeld ? ' held' : ''}`} onClick={prop.holdDice}>
+      {[...Array(9)].map((_, i) => (
+        <div key={i} className={`pip${pipMap[prop.value].includes(i) ? ' active' : ''}`}></div>
+      ))}
+    </div>
+    );
 }
+
